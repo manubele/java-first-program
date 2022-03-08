a3b649ae019d3b17f274d773c4d8da43253ea1ef
Mithz <mithileshbele2002@gmail.com>
2022-03-08 20:33:50 +0530

    Completed the project

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -11,7 +11,7 @@ public class BestLoanRates {
     );
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your Name");
+        System.out.println("Enter your name");
         String name = scanner.nextLine();
         System.out.println("Hello" +name);
 
